2a7deab21a60de03997a098d2e74cf6414a5660f
Seán Cronin <147072029+SeanC345@users.noreply.github.com>
2025-12-03 14:32:47 +0000

    Added full backend TMDB integration and movie API endpoints

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import { getMovies } from '../tmdb-api'; 
+import { getMovie } from '../tmdb-api';
+import { getGenres } from '../tmdb-api';
+import { getMovieImages } from '../tmdb-api';
+import { getMovieReviews } from '../tmdb-api';
+import { getUpcomingMovies } from '../tmdb-api';
+import { getNowPlayingMovies } from '../tmdb-api';
+import { getPopularMovies } from '../tmdb-api';
+import { getTopRatedMovies } from '../tmdb-api';
+import { getTrendingMovies } from '../tmdb-api';
+import { getMovieCredits } from '../tmdb-api';
+import { getSimilarMovies } from '../tmdb-api';
+import { getRecommendedMovies } from '../tmdb-api';
+
 
 
 const router = express.Router();
@@ -8,6 +21,66 @@ const router = express.Router();
 router.get('/discover', asyncHandler(async (req, res) => {
     const discoverMovies = await getMovies();
     res.status(200).json(discoverMovies);
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+  const data = await getMovie(req.params.id);
+  res.status(200).json(data);
+}));
+
+router.get('/:id/images', asyncHandler(async (req, res) => {
+  const data = await getMovieImages(req.params.id);
+  res.status(200).json(data);
+}));
+
+router.get('/:id/reviews', asyncHandler(async (req, res) => {
+  const data = await getMovieReviews(req.params.id);
+  res.status(200).json(data);
+}));
+
+router.get('/upcoming', asyncHandler(async (req, res) => {
+  const data = await getUpcomingMovies();
+  res.status(200).json(data);
+}));
+
+router.get('/trending', asyncHandler(async (req, res) => {
+  const data = await getTrendingMovies();
+  res.status(200).json(data);
+}));
+
+router.get('/popular', asyncHandler(async (req, res) => {
+  const data = await getPopularMovies();
+  res.status(200).json(data);
+}));
+
+router.get('/top_rated', asyncHandler(async (req, res) => {
+  const data = await getTopRatedMovies();
+  res.status(200).json(data);
+}));
+
+router.get('/now_playing', asyncHandler(async (req, res) => {
+  const data = await getNowPlayingMovies();
+  res.status(200).json(data);
+}));
+
+router.get('/:id/recommendations', asyncHandler(async (req, res) => {
+  const data = await getRecommendedMovies(req.params.id);
+  res.status(200).json(data);
+}));
+
+router.get('/:id/similar', asyncHandler(async (req, res) => {
+  const data = await getSimilarMovies(req.params.id);
+  res.status(200).json(data);
+}));
+
+router.get('/:id/credits', asyncHandler(async (req, res) => {
+  const data = await getMovieCredits(req.params.id);
+  res.status(200).json(data);
+}));
+
+router.get('/genres', asyncHandler(async (req, res) => {
+  const data = await getGenres();
+  res.status(200).json(data);
 }));
 
 
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -11,3 +11,171 @@ export const getMovies = async () => {
 
     return await response.json();
 };
+
+export const getMovie = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_KEY}&language=en-US&append_to_response=credits`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getGenres = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getMovieImages = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.TMDB_KEY}`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getMovieReviews = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.TMDB_KEY}`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getUpcomingMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getNowPlayingMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getPopularMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getTopRatedMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getTrendingMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getMovieCredits = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.TMDB_KEY}`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getSimilarMovies = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getRecommendedMovies = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getPerson = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.TMDB_KEY}&language=en-US`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
+
+export const getPersonMovieCredits = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${process.env.TMDB_KEY}&language=en-US`
+    );
+
+    if (!response.ok) {
+        throw new Error((await response.json()).status_message || "Something went wrong");
+    }
+
+    return await response.json();
+};
