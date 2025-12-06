b2a519028fc82a4297dc142cec0865508a7762d3
Seán Cronin <147072029+SeanC345@users.noreply.github.com>
2025-12-06 14:26:00 +0000

    Fixed MyReview Page to render with login

--- react-movies/src/pages/myReviewsPage.jsx
+++ react-movies/src/pages/myReviewsPage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import { getMyReviews, removeReview } from "../api/tmdb-api";
+import { useQuery, useQueries, useQueryClient } from "@tanstack/react-query";
+import { getMyReviews, removeReview, getMovie } from "../api/tmdb-api";
 import PageTemplate from "../components/templateMovieListPage";
 import Spinner from "../components/spinner";
-import Button from "@mui/material/Button";
 import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const MyReviewsPage = () => {
   const [snack, setSnack] = React.useState("");
-  
-  const { data, isPending, isError, error } = useQuery({
+  const queryClient = useQueryClient();
+
+ 
+  const { data: reviews, isPending, isError, error } = useQuery({
     queryKey: ["myReviews"],
     queryFn: getMyReviews
   });
@@ -17,22 +20,40 @@ const MyReviewsPage = () => {
   if (isPending) return <Spinner />;
   if (isError) return <h1>{error.message}</h1>;
 
+  const movieQueries = useQueries({
+    queries: reviews.map((r) => ({
+      queryKey: ["movie", { id: r.movieId }],
+      queryFn: getMovie
+    }))
+  });
+
+  const loadingMovie = movieQueries.some((mq) => mq.isPending);
+  if (loadingMovie) return <Spinner />;
+
+  const movies = movieQueries.map((mq, index) => {
+    const movie = mq.data;
+    return {
+      ...movie,
+      _review: reviews[index] 
+    };
+  });
+
   return (
     <>
       <PageTemplate
         title="My Reviews"
-        movies={data}
-        action={(review) => (
-          <Button
+        movies={movies}
+        action={(movie) => (
+          <IconButton
             color="error"
-            variant="outlined"
             onClick={async () => {
-              await removeReview(review._id);
+              await removeReview(movie._review._id);
               setSnack("Review deleted");
+              queryClient.invalidateQueries(["myReviews"]);
             }}
           >
-            Delete
-          </Button>
+            <DeleteIcon />
+          </IconButton>
         )}
       />
 
